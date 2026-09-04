fd7da64abb3fe0a38efbfe84430fadb21cff971e
yersi alviarez <yersialviarez@gmail.com>
2026-09-04 12:25:52 -0500

    test: add edge-case empty fields and navigation link tests for register page

--- pages/register_page.py
+++ pages/register_page.py
@@ -11,31 +11,63 @@ class RegisterPage(BasePage):
     def url(self) -> str:
         """Return the register page URL."""
         return "/register"
-    
+
+    #------------------------------------------------
     @property
     def register_name(self) -> Locator:
         """Return Register Name field locator."""
         return self.page.get_by_test_id("register-name")
-    
+
+    @property
+    def name_alert(self) -> Locator:
+        """Return name alert locator."""
+        return self.page.get_by_test_id("error-name")
+
+    #--------------------------------------------------
     @property
     def register_email(self) -> Locator:
         """Return register email field locator."""
         return self.page.get_by_test_id("register-email")
-    
+
+    @property
+    def email_alert(self) -> Locator:
+        """Return email alert locator."""
+        return self.page.get_by_test_id("error-email")
+
+
+    #--------------------------------------------------
     @property
     def register_password(self) -> Locator:
         """Return register password field locator."""
         return self.page.get_by_test_id("register-password")
-    
+
+    @property
+    def password_alert(self) -> Locator:
+        """Return password alert locator."""
+        return self.page.get_by_test_id("error-password")
+
+    #------------------------------------------------
     @property
     def confirm_password(self) -> Locator:
         """Return confirm password field locator."""
         return self.page.get_by_test_id("register-confirm")
 
     @property
+    def confirm_alert(self) -> Locator:
+        """Return confirm password alert locator."""
+        return self.page.get_by_test_id("error-confirm")
+
+    #-------------------------------------------------
+
+    @property
     def create_account_btn(self) -> Locator:
         """Return create count submit button locator."""
         return self.page.get_by_test_id("register-submit")
+
+    @property
+    def sign_in_link(self) -> Locator:
+        """Return the sign in link locator."""
+        return self.page.get_by_test_id("login-link")
 
     def register_account(self, name: str, email: str, password: str, confirm_password: str) -> None:
         """Perform full register action."""
--- tests/test_register/test_register.py
+++ tests/test_register/test_register.py
@@ -1,4 +1,5 @@
 """Tests from  Register UI."""
+from urllib import response
 
 import allure
 import pytest
@@ -10,9 +11,9 @@ from config.settings import Settings
 class TestRegister:
     """Tests Register UI."""
 
-    @pytest.mark.register
+
     @pytest.mark.smoke
-    def test_register_user(self, register_page: RegisterPage, page: Page, settings: Settings):
+    def test_register_user(self, register_page: RegisterPage, page: Page):
         """Verify that a new user can register successfully."""
         #Data new user
         uid = uuid.uuid4().hex[:8]
@@ -30,3 +31,122 @@ class TestRegister:
         )
 
         expect(page).to_have_url("/catalog")
+
+
+    @pytest.mark.regression
+    def test_register_with_empty_name_field(self,register_page: RegisterPage):
+        """Empty name field should return name error alert and not create account."""
+        #Data new user
+        uid = uuid.uuid4().hex[:8]
+        user_name = ""
+        user_email = f"user_{uid}@shopease.io"
+        user_password = "Password123!"
+
+        #Navigate to register
+        register_page.navigate()
+        register_page.register_account(
+            name=user_name,
+            email=user_email,
+            password=user_password,
+            confirm_password=user_password
+        )
+
+        expect(register_page.name_alert).to_be_visible()
+
+
+    @pytest.mark.regression
+    def test_register_with_empty_email_field(self,register_page: RegisterPage):
+        """Empty email field should return email error alert and not create account."""
+        #Data new user
+        uid = uuid.uuid4().hex[:8]
+        user_name = f"user {uid}"
+        user_email = ""
+        user_password = "Password123!"
+
+        #Navigate to register
+        register_page.navigate()
+        register_page.register_account(
+            name=user_name,
+            email=user_email,
+            password=user_password,
+            confirm_password=user_password
+        )
+
+        expect(register_page.email_alert).to_be_visible()
+
+
+    @pytest.mark.regression
+    def test_register_with_empty_password_field(self, register_page: RegisterPage):
+        """Empty password field should return password alert and not create account."""
+        #Data new user
+        uid = uuid.uuid4().hex[:8]
+        user_name = f"user {uid}"
+        user_email = f"user{uid}@shopease.io"
+        user_password = ""
+
+        #Navigate to register
+        register_page.navigate()
+        register_page.register_account(
+            name=user_name,
+            email=user_email,
+            password=user_password,
+            confirm_password=user_password
+        )
+
+        expect(register_page.password_alert).to_be_visible()
+
+
+    @pytest.mark.regression
+    def test_register_with_empty_confirm_password_field(self, register_page: RegisterPage):
+        """Empty confirm password field should return confirm password alert and not create account."""
+        #Data new user
+        uid = uuid.uuid4().hex[:8]
+        user_name = f"user {uid}"
+        user_email = f"user{uid}@shopease.io"
+        user_password = "Password123!"
+
+        #Navigate to register
+        register_page.navigate()
+
+        register_page.register_account(
+            name=user_name,
+            email=user_email,
+            password=user_password,
+            confirm_password=""
+        )
+
+        expect(register_page.confirm_alert).to_be_visible()
+
+
+    @pytest.mark.regression
+    def test_register_with_all_empty_fields(self, register_page: RegisterPage):
+        """All empty required fields should return name alert/email alert/password alert
+        and not create account."""
+
+        #Navigate to register
+        register_page.navigate()
+
+        register_page.register_account(
+            name="",
+            email="",
+            password="",
+            confirm_password=""
+        )
+
+        expect(register_page.name_alert).to_be_visible()
+        expect(register_page.email_alert).to_be_visible()
+        expect(register_page.password_alert).to_be_visible()
+
+
+    @pytest.mark.register
+    @pytest.mark.regression
+    def test_sign_in_link(self, register_page: RegisterPage, page: Page):
+        """ The 'Sign in' link must be redirected to login page."""
+        #Navigate to register
+        register_page.navigate()
+
+        register_page.sign_in_link.click()
+
+        expect(page).to_have_url("/login")
+
+
